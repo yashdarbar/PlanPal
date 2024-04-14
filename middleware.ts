a99560b9ee3ad998adba15f1460843ebb27064aa
Yash Darbar <darbaryash44@gmail.com>
2024-04-15 03:22:28 +0530

    commit

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,14 @@ export function middleware(request: NextRequest) {
 
     const path = request.nextUrl.pathname;
 
-    const isPublic = path === "/login" || path === "/signup";
+    const isPublicPath = path === "/login" || path === "/signup";
 
     const token = request.cookies.get("token")?.value || "";
 
-    if (isPublic && token) {
+    if (isPublicPath && token) {
         return NextResponse.redirect(new URL("/addTask", request.url));
     }
-    if (!isPublic && !token) {
+    if (!isPublicPath && !token) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
