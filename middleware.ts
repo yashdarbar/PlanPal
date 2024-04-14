a5a9b18203cc09b638b0cc83f4dbcdc4d51e0a4e
Yash Darbar <darbaryash44@gmail.com>
2024-04-15 03:10:31 +0530

    middleware done

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+    const path = request.nextUrl.pathname;
+
+    const isPublic = path === "/login" || path === "/signup";
+
+    const token = request.cookies.get("token")?.value || "";
+
+    if (isPublic && token) {
+        return NextResponse.redirect(new URL("/addTask", request.url));
+    }
+    if (!isPublic && !token) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ['/login', '/signup', '/addTask', '/showTask', '/']};
